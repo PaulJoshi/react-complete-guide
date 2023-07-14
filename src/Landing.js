6f3d950fba2eba75c35dbcb4e197731d7dae64b7
Paul Joshi <pauljoshi@gmail.com>
2023-07-14 11:46:22 +0530

    🖼️ Thumbnail Placeholders

--- src/Landing.js
+++ src/Landing.js
@@ -11,8 +11,8 @@ const Landing = () => {
 				<Link
 					to={project.path}
 				>
-					<div className="flex flex-col items-center border border-teal-100 shadow rounded-sm bg-teal-100 transition hover:shadow-md hover:bg-teal-200 hover:border-teal-200">
-						<img className="h-full w-full" src={`${screenshotUrl}${project.path}&type=png`} alt="project-thumbnail"/>
+					<div className="flex flex-col items-center border border-teal-100 shadow rounded-sm bg-teal-100 transition hover:shadow-lg hover:bg-teal-200 hover:border-teal-200">
+						<img className="h-48 w-full object-cover bg-teal-50" src={`${screenshotUrl}${project.path}&type=png`} alt="project-thumbnail"/>
 						<p className="p-4 md:text-lg text-teal-950 font-semibold">{project.name}</p>
 					</div>
 				</Link>
